0157f2736a75b1bc3ef090719d1b97fcbc3c50c8
Sunny <sunny@Imacs-iMac.local>
2022-06-03 16:43:36 +0700

    Add hover to menu2

--- vue/src/components/Sidebar.vue
+++ vue/src/components/Sidebar.vue
@@ -19,7 +19,9 @@
   >
     <div class="w-full h-20 border-b flex px-4 items-center mb-8">
       <p class="font-semibold text-2xl text-blue-400 pl-4">
-        EMPLOYEE MANAGEMENT
+        <router-link :to="{ name: 'Dashboard' }"
+          >EMPLOYEE MANAGEMENT</router-link
+        >
       </p>
     </div>
 
