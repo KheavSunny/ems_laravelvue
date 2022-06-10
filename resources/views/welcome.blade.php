c798aed387386a177053fc668a9f6718ad17ac3b
Sunny <sunny@Imacs-iMac.local>
2022-06-10 14:41:22 +0700

    githubtoheroku3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -405,7 +405,6 @@
 </head>
 
 <body class="antialiased">
-    <div>Testting</div>
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
